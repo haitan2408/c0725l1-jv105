37ae7141d6a31cb9959d90ddbcc8cab6f6c950a5
haitan2408 <haitan28102408@gmail.com>
2025-08-13 20:57:50 +0700

    up

---
+++ ss6_js_overview/demo.js
@@ -0,0 +1,20 @@
+document.getElementById("btn-like").addEventListener("click", function () {
+
+    this.style.backgroundColor='blue';
+    this.style.color='white'
+    this.innerText = "Dislike"
+    // document.writeln("hello")
+
+    // let a = 5;
+//     Nhập
+    /*
+    - prompt: hiện 1 cửa sổ để nhập
+    - confirm: cho phép người dùng lựa chọn xác nhận hoặc đóng
+     */
+//     Xuất
+    /*
+    - alert: hiển thị 1 hộp thoại thông báo
+    - document.write: viết lên tài liệu html
+    - console.log: chỉ dành cho dev.
+     */
+})
